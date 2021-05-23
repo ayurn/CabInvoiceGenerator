70c1119930cff918c78643beffe2545c3f834cb7
ayurn <65171090+ayurn@users.noreply.github.com>
2021-05-23 18:38:18 +0530

    Added : given user id returns invoice

--- src/main/java/CabInvoiceGenerator.java
+++ src/main/java/CabInvoiceGenerator.java
@@ -1,5 +1,6 @@
 public class CabInvoiceGenerator {
 
+    RideRepository rideRepository = new RideRepository();
     private final int COST_PER_KM = 10;
     private final int COST_PER_MINUTE = 1;
     public int MIN_FARE = 5;
@@ -31,5 +32,13 @@ public class CabInvoiceGenerator {
         double numberOfRides = rides.length;
         double averageFare = totalFare/numberOfRides;
         return averageFare;
+    }
+
+    public double getRidesDetails(String userID) {
+        return this.calculateFare(rideRepository.getRides(userID));
+    }
+
+    public void addRides(String userID, Rides[] rides) {
+        rideRepository.addRides(userID, rides);
     }
 }
